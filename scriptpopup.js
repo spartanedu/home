4de76447c8eb856cb06e51ed5b0cfaf0e434bc57
Spartan <67495463+asumadukwabenasakyi@users.noreply.github.com>
2023-01-28 19:03:26 -0600

    Add files via upload

---
+++ scriptpopup.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+	setTimeout(
+		function open(event){
+			document.querySelector(".popup").style.
+			display = "block";
+		},
+		4000
+	)
+});
+
+
+document.querySelector("#close").addEventListener
+("click", function(){
+	document.querySelector(".popup").
+	style.display = "none";
+});
